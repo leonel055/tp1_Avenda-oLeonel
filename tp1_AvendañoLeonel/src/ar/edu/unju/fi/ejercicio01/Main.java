de95f599f145956187ebb533ea4ef1652b184075
none <none@DESKTOP-FKH17L5>
2024-04-04 17:34:27 -0300

    ejercicio1

---
+++ tp1_AvendañoLeonel/src/ar/edu/unju/fi/ejercicio01/Main.java
@@ -0,0 +1,16 @@
+package ar.edu.unju.fi.ejercicio01;
+
+public class Main {
+	
+			public static void main (String[] args) {
+				
+				float num1 = 4;
+				float num2 = 8;
+				float num3 = 6;
+				float num4 = 9;
+				float num5 = 10;
+				float promedio = (num1 + num2 + num3 + num4 + num5) / 5;
+				System.out.println(num1+ ", "+num2+", "+num3+", "+num4+", "+num5);
+				System.out.println("El promedio es: "+promedio);
+			}
+	}
