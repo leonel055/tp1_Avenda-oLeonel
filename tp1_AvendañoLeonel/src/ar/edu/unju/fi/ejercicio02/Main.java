f7db08a9e50def63fc4c5744791c3284f96a8a17
leonel055 <166062045+leonel055@users.noreply.github.com>
2024-04-13 21:03:22 -0300

    Add files via upload

---
+++ tp1_AvendañoLeonel/src/ar/edu/unju/fi/ejercicio02/Main.java
@@ -0,0 +1,24 @@
+package ar.edu.unju.fi.ejercicio02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String NombrePais = "Argentina";
+        int EdadPersona = 18;
+        double AlturaEdificio = 50.5; // 
+        double PrecioProducto = 9.99; 
+        String NumeroTelefono = "+54123456789";
+        double Coseno = Math.cos(0.5); 
+
+   
+        System.out.println("Nombre del país: " + NombrePais);
+        System.out.println("Edad de la persona: " + EdadPersona + " años");
+        System.out.println("Altura del edificio: " + AlturaEdificio + " mts");
+        System.out.println("Precio del producto: " + "$ " + PrecioProducto );
+        System.out.println("Número de teléfono: " + NumeroTelefono);
+        System.out.println("Coseno de 0.5: " + Coseno);
+
+	}
+
+}
